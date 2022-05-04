6262be55c49758ec276e4eb1ece71dd36818e30e
heronfarias <104874561+heronfarias@users.noreply.github.com>
2022-05-03 21:34:55 -0300

    Create moto.java

---
+++ moto.java
@@ -0,0 +1,16 @@
+package Atos;
+
+ public class moto {
+	String Marca;
+	String Modelo;
+	String Cilindradas;
+
+ public void atribuirValores(String Marca, String Modelo, String Cilindradas) {
+
+	}
+
+ public void retornarMoto() {
+	System.out.println("Moto: "+Marca+" "+Modelo+" "+Cilindradas+"cc");
+}
+
+}
